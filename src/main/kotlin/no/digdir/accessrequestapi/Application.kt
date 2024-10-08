a64195090851f2ab0064e9e29ec98b9897a3aa93
halvor <halvorbmundal@gmail.com>
2024-10-08 09:52:17 +0200

    Remove MongoAutoConfiguration and MongoDataAutoConfiguration imports

--- src/main/kotlin/no/digdir/accessrequestapi/Application.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/Application.kt
@@ -2,8 +2,6 @@ package no.digdir.accessrequestapi
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
