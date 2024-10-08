689c90af40193d444da9f5be86293ce7edf28228
halvor <halvorbmundal@gmail.com>
2024-10-08 09:52:47 +0200

    Remove EnableScheduling annotation

--- src/main/kotlin/no/digdir/accessrequestapi/Application.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/Application.kt
@@ -3,10 +3,8 @@ package no.digdir.accessrequestapi
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 @ConfigurationPropertiesScan
 open class Application
 
