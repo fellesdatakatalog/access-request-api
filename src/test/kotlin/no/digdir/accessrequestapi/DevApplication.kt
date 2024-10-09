cf9485668ad50b4c85438e8a149dbba1baec1733
halvor <halvorbmundal@gmail.com>
2024-10-09 16:29:10 +0200

    Remove DevApplication.kt

--- src/test/kotlin/no/digdir/accessrequestapi/DevApplication.kt
+++
@@ -1,8 +0,0 @@
-package no.digdir.accessrequestapi
-
-import org.springframework.boot.SpringApplication
-
-fun main(args: Array<String>) {
-    System.setProperty("spring.profiles.active", "dev")
-    SpringApplication.run(Application::class.java, *args)
-}
