45299a3d81b52d13600f8670cc00ac58b7e72e30
halvor <halvorbmundal@gmail.com>
2024-10-09 10:55:46 +0200

    Forward 'not found' response if the resource does not exist

--- src/main/kotlin/no/digdir/accessrequestapi/controller/HandlekurvController.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/controller/HandlekurvController.kt
@@ -3,11 +3,15 @@ package no.digdir.accessrequestapi.controller
 import no.digdir.accessrequestapi.client.FelleskatalogClient
 import no.digdir.accessrequestapi.model.Handlekurv
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import java.util.UUID
 
 @RestController
@@ -17,6 +21,10 @@ class HandlekurvController(
     val fellesdatakatalogFrontendUrl: String,
     val felleskatalogClient: FelleskatalogClient,
 ) {
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
+    @ExceptionHandler(WebClientResponseException.NotFound::class)
+    fun handleNotFound() {}
+
     @GetMapping("/{type}/{id}")
     fun getHandlekurv(
         @PathVariable type: String,
