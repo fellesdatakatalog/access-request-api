29f0a85a7ce47c3f1f958c0f1abbcac82b13cc28
halvor <halvorbmundal@gmail.com>
2024-12-16 16:06:00 +0100

    Legg til retry mot fdk urls

--- src/main/kotlin/no/digdir/accessrequestapi/client/FelleskatalogClient.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/client/FelleskatalogClient.kt
@@ -1,11 +1,13 @@
 package no.digdir.accessrequestapi.client
 
+import java.time.Duration
 import no.digdir.accessrequestapi.configuration.FdkUrls
 import no.digdir.accessrequestapi.model.DataResourceMetadata
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import java.util.UUID
+import reactor.util.retry.Retry
 
 @Component
 class FelleskatalogClient(
@@ -21,11 +23,19 @@ class FelleskatalogClient(
     ): DataResourceMetadata? {
         logger.info("Fetching metadata for type: $type and id: $id from Felleskatalog.")
 
-        return webClient
+        val metadata = webClient
             .get()
             .uri("/$type/$id")
             .retrieve()
             .bodyToMono<DataResourceMetadata>()
+            .retryWhen(
+                Retry.backoff(3, Duration.ofSeconds(2))
+                        .doBeforeRetry { logger.info("Retrying due to: ", it.failure()) }
+            )
             .block()
+
+        logger.info("Metadata fetched successfully: $metadata")
+
+        return metadata
     }
 }
