093facdf53832fec07dcb84fce629622af976690
halvor <halvorbmundal@gmail.com>
2024-12-18 09:29:25 +0100

    Reduser backoff tid

--- src/main/kotlin/no/digdir/accessrequestapi/client/FelleskatalogClient.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/client/FelleskatalogClient.kt
@@ -1,13 +1,13 @@
 package no.digdir.accessrequestapi.client
 
-import java.time.Duration
 import no.digdir.accessrequestapi.configuration.FdkUrls
 import no.digdir.accessrequestapi.model.DataResourceMetadata
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import java.util.UUID
 import reactor.util.retry.Retry
+import java.time.Duration
+import java.util.UUID
 
 @Component
 class FelleskatalogClient(
@@ -23,16 +23,17 @@ class FelleskatalogClient(
     ): DataResourceMetadata? {
         logger.info("Fetching metadata for type: $type and id: $id from Felleskatalog.")
 
-        val metadata = webClient
-            .get()
-            .uri("/$type/$id")
-            .retrieve()
-            .bodyToMono<DataResourceMetadata>()
-            .retryWhen(
-                Retry.backoff(3, Duration.ofSeconds(2))
-                        .doBeforeRetry { logger.info("Retrying due to: ", it.failure()) }
-            )
-            .block()
+        val metadata =
+            webClient
+                .get()
+                .uri("/$type/$id")
+                .retrieve()
+                .bodyToMono<DataResourceMetadata>()
+                .retryWhen(
+                    Retry
+                        .backoff(3, Duration.ofMillis(500))
+                        .doBeforeRetry { logger.info("Retrying due to: ${it.failure().message}", it.failure()) },
+                ).block()
 
         logger.info("Metadata fetched successfully: $metadata")
 
