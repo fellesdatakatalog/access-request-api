a5c8b49783d27715e3c3a43146734fee858a3214
torleifg <torgravels@gmail.com>
2025-03-12 18:26:10 +0100

    chore: dependencies and code cleanup

--- src/main/kotlin/no/digdir/accessrequestapi/client/KudafClient.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/client/KudafClient.kt
@@ -2,18 +2,15 @@ package no.digdir.accessrequestapi.client
 
 import no.digdir.accessrequestapi.configuration.KudafUrls
 import no.digdir.accessrequestapi.model.ShoppingCart
+import org.slf4j.Logger
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import reactor.util.retry.Retry
-import java.time.Duration
 
 @Component
-class KudafClient(
-    kudafUrls: KudafUrls,
-) {
-    val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
-    val webClient = WebClient.create(kudafUrls.soknadApi)
+class KudafClient(kudafUrls: KudafUrls) {
+    private val logger: Logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+    private val webClient: WebClient = WebClient.create(kudafUrls.soknadApi)
 
     fun getRedirectUrl(cart: ShoppingCart): String? {
         logger.info("Fetching redirect URL for cart: $cart")
--- src/main/kotlin/no/digdir/accessrequestapi/controller/KudafResolverController.kt
+++ src/main/kotlin/no/digdir/accessrequestapi/controller/KudafResolverController.kt
@@ -1,31 +1,26 @@
 package no.digdir.accessrequestapi.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
-import no.digdir.accessrequestapi.client.FelleskatalogClient
+import no.digdir.accessrequestapi.client.FdkClient
 import no.digdir.accessrequestapi.configuration.FdkUrls
 import no.digdir.accessrequestapi.model.DataResourceMetadata
 import no.digdir.accessrequestapi.model.DatasetLanguage
 import no.digdir.accessrequestapi.model.ShoppingCart
+import org.slf4j.Logger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.client.WebClientResponseException
-import java.util.UUID
+import java.util.*
 
 @Tag(name = "DataDef resolver")
 @RestController
 @RequestMapping(value = ["/datadef-resolver"], produces = ["application/json"])
 class KudafResolverController(
-    val felleskatalogClient: FelleskatalogClient,
-    val fdkUrls: FdkUrls,
+    private val fdkClient: FdkClient,
+    private val fdkUrls: FdkUrls
 ) {
-    val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+    private val logger: Logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
     @ExceptionHandler(WebClientResponseException.NotFound::class)
@@ -44,14 +39,14 @@ class KudafResolverController(
         val resourceType = dataDef.type
 
         val metadata =
-            felleskatalogClient.getMetadata(resourceType.toUrlString(), resourceId) ?: return ResponseEntity.notFound().build()
+            fdkClient.getMetadata(resourceType.toUrlString(), resourceId) ?: return ResponseEntity.notFound().build()
 
         return ResponseEntity.ok(
             ResolveDataDefResponse(
                 metadata = metadata,
                 language = language,
-                urlToResource = "${fdkUrls.frontend}/$resourceType/$resourceId",
-            ),
+                urlToResource = "${fdkUrls.frontend}/$resourceType/$resourceId"
+            )
         )
     }
 }
@@ -61,13 +56,13 @@ data class ResolveDataDefResponse(
     val title: String?,
     val description: String?,
     val urlToResource: String?,
-    val hintIsPrePublicationData: Boolean,
+    val hintIsPrePublicationData: Boolean
 ) {
     constructor(metadata: DataResourceMetadata, language: DatasetLanguage, urlToResource: String?) : this(
         dataResourceId = metadata.id,
         title = metadata.title.get(language),
         description = metadata.description?.get(language),
         urlToResource = urlToResource,
-        hintIsPrePublicationData = metadata.isDatasetWithoutDistribution(),
+        hintIsPrePublicationData = metadata.isDatasetWithoutDistribution()
     )
 }
